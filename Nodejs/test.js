6627e943271252ddd71d2adf2306c9698dfef850
Wietze <wietzeslagman@gmail.com>
2015-06-25 15:30:10 +0200

    Update test.js

--- Nodejs/test.js
+++ Nodejs/test.js
@@ -1,3 +1,8 @@
+/*
+ * Code taken from: http://www.instructables.com/member/Patrick+S/
+ * at: http://www.instructables.com/id/Autonomous-AR-Parrot-Drone-20-Flying/step8/Streaming-Video/
+*/
+
 var arDrone = require('ar-drone');
 var client = arDrone.createClient();
 
